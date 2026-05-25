c4f15b04a023212836d0aa0aac299b1655aec542
Robin <robin.frydig@hotmail.com>
2026-05-25 22:25:22 +0200

    project setup

---
+++ src/main/java/com/buddygoals/buddygoals_backend/BuddygoalsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.buddygoals.buddygoals_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddygoalsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddygoalsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buddygoals/buddygoals_backend/BuddygoalsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buddygoals.buddygoals_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddygoalsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
